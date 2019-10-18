bfed2862109c9163f115790536bc5fe112bc058e
pradeepkala <pradeepkala32@gmail.com>
2019-10-18 17:01:45 +0530

    changed on feature branch

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          Hi there. Am I changeed. Here you go.
+          Hi there. Am I changed in the feature branch. Here you go.
         </p>
       </div>
     );
